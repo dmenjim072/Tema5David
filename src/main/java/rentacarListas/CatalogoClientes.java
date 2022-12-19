6286c6639927711561dbde1b8c4d9f5b1f9bc9e5
dmenjim072 <dmenjim072@g.educaand.es>
2022-12-19 14:35:23 +0100

    asd

--- src/main/java/rentacarListas/CatalogoClientes.java
+++ src/main/java/rentacarListas/CatalogoClientes.java
@@ -4,7 +4,9 @@
  */
 package rentacarListas;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  *
@@ -12,48 +14,28 @@ import java.util.Arrays;
  */
 public class CatalogoClientes {
     
-    private int numeroClientes;
-    private Clientes[] listaClientes;//Creamos un array de clientes
+    private List<Clientes> lista;
 
     //Constructor dando tamaño
     public CatalogoClientes(int tamaño){
         
         tamaño = Math.abs(tamaño);//Para pasarlo a positivo
         
-        this.numeroClientes = 0;
-        this.listaClientes = new Clientes[tamaño];
+        this.lista = new ArrayList<>(tamaño);
         
-    }
-    
-    //Metodo para añadir clientes al catalogo
-    public void añadirClientes(Clientes x){
-        
-        //Si hay hueco se inserta en el hueco
-        if(this.numeroClientes<listaClientes.length){
-            
-            //Recorremos el array para añadirle clientes al catalogo
-            for (int i = 0; i < listaClientes.length; i++) {
-                
-                //Elejimos la posicion(la primera que este vacia)
-                if(this.listaClientes[i] == null){
-                    this.listaClientes[i] = x;
-                    this.numeroClientes++;
-                    System.out.println("Guardando cliente en posicion: " + i);
-                    break;
-                }
-            }
-        }else{//Si no hay hueco creamos un array nuevo copiando el actual y añadiendo un hueco mas
-            this.listaClientes = Arrays.copyOf(listaClientes, ++numeroClientes);
+        //Una vez creada la estructura de datos le doy valor a cada elemento(de forma aleatoria)
+        for (int i = 0; i < tamaño; i++) {
+            this.lista.add(new Clientes());
         }
         
-    } 
+    }    
     
     //Metodo para buscar un cliente en concreto
      private int buscarCliente(Clientes c) {
         //Búsqueda secuencial
         if (c != null) {
-            for (int i = 0; i < this.listaClientes.length; i++) {
-                if (this.listaClientes[i] != null && c.equals(this.listaClientes[i])) {
+            for (int i = 0; i < this.lista.size(); i++) {
+                if (c.equals(this.lista.get(i))) {
                     return i;
                 }
             }
@@ -65,7 +47,7 @@ public class CatalogoClientes {
         Clientes aux = new Clientes();
         aux.setNIF(nif);
         int posicion = buscarCliente(aux);
-        return (posicion >= 0) ? this.listaClientes[posicion] : null;
+        return (posicion >= 0) ? this.lista.get(posicion) : null;
     }
         
     //Metodo para borrar vehiculo
@@ -103,13 +85,13 @@ public class CatalogoClientes {
     }
     
     //Metodo para mostrar el catalogo
-    public void mostrarCatalogo(CatalogoClientes c){
-        
-        for (int i = 0; i < listaClientes.length; i++) {
-            System.out.println(listaClientes[i]);
-        }
-        
-    } 
+//    public void mostrarCatalogo(CatalogoClientes c){
+//        
+//        for (int i = 0; i < listaClientes.length; i++) {
+//            System.out.println(listaClientes[i]);
+//        }
+//        
+//    } 
                     
 }
 
