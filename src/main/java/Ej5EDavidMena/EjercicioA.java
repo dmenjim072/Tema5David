4ff94c50304a8fc507ea1e41c1eb777bc9bd96d1
David <dmenjim072@g.educaand.es>
2023-02-03 07:38:46 +0100

    asd

--- src/main/java/Ej5EDavidMena/EjercicioA.java
+++ src/main/java/Ej5EDavidMena/EjercicioA.java
@@ -4,6 +4,10 @@
  */
 package Ej5EDavidMena;
 
+import java.util.InputMismatchException;
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author david
@@ -12,8 +16,261 @@ public class EjercicioA {
     
     public static void main(String[] args) {
         
+        Scanner teclado = new Scanner(System.in);
         
+        int opcion;
+        int tamanio;
+        int[][] matrizA;
+        Coordenada coordenada;
         
+        //Menu que se va a mostrar al usuario
+        String menu = """
+                   
+                   1-Introducir numeros por el usuario
+                   2-Numeros aleatorios 
+                   3-Salir
+                      
+                   """;
+        
+        do {//Si la opcion es 3 se sale del programa
+            //Imprimo el menu y leo la opcion
+            System.out.println(menu);
+            opcion = teclado.nextInt();
+            
+            switch (opcion) {
+                case 1:
+
+                    //Llamo al metodo para pedir tamanio al usuario
+                    tamanio = pedirTamanio();
+
+                    //Creo la matriz con el tamanio preguntado anteriormente
+                    matrizA = new int[tamanio][tamanio];
+
+                    //Relleno la matriz con valores que me da el usuario
+                    rellenarConUsuario(matrizA);
+
+                    //Imprimo la matriz ya rellena
+                    imprimirMatriz(matrizA);
+                    
+                    //Igualo el objeto coordenada a el resultado obtenido del metodo
+                    coordenada = coordenadaNumero(matrizA);
+                    
+                    //Imprimo la coordenada
+                    System.out.println("El numero se encuentra en la posicion " + coordenada.getFila() + ", " + coordenada.getColumna());
+                    
+                    break;
+                
+                case 2:
+
+                    //Llamo al metodo para pedir tamanio al usuario
+                    tamanio = pedirTamanio();
+
+                    //Creo la matriz con el tamanio preguntado anteriormente
+                    matrizA = new int[tamanio][tamanio];
+
+                    //Relleno la matriz con valores aleatorios entre 0 y 20
+                    rellenarAleatorio(matrizA);
+
+                    //Imprimo la matriz ya rellena
+                    imprimirMatriz(matrizA);
+                    
+                    //Igualo el objeto coordenada a el resultado obtenido del metodo
+                    coordenada = coordenadaNumero(matrizA);
+                    
+                    //Imprimo la coordenada
+                    System.out.println("El numero se encuentra en la posicion " + coordenada.getFila() + ", " + coordenada.getColumna());
+                    
+                    break;
+                
+            }
+            
+        } while (opcion != 3);
+        
+    }
+    
+    private static int pedirTamanio() { //Metodo para pedir el tamanio de la matriz
+
+        Scanner teclado = new Scanner(System.in);
+        
+        int tamanio = 0;
+        
+        do {
+            
+            try {
+                
+                System.out.println("Dime un numero entero entre 3 y 6");
+                tamanio = teclado.nextInt();
+
+                //Si el tamaño es menor que 3 y mayor que 6 se le preguntara
+                //al usuario otra vez por los valores hasta que los introduzca
+                //correctamente
+                if (tamanio < 3 || tamanio > 6) {
+                    
+                    System.out.println("Por favor elige un numero entero entre 3 y 6");
+                }
+
+                //Captura la excepcion por si el usuario no mete un valor entero
+            } catch (InputMismatchException ime) {
+                
+                System.out.println("Por favor elige un numero entero entre 3 y 6");
+                
+            }
+            
+            teclado.nextLine();
+            
+        } while (tamanio < 3 || tamanio > 6);
+        
+        return tamanio; //Devuelvo el tamanio de la matriz
+    }
+
+    //Relleno la matriz con numeros aleatorios entre 0 y 20
+    private static void rellenarAleatorio(int[][] matrizA) {
+        
+        Scanner teclado = new Scanner(System.in);
+        
+        Random numeroAleatorio = new Random();
+        
+        for (int i = 0; i < matrizA.length; i++) {
+            
+            for (int j = 0; j < matrizA[i].length; j++) {
+
+                //Genero un numero aleatorio entre 0 y 20
+                int nAleatorio = numeroAleatorio.nextInt(0, 20 + 1);
+                
+                matrizA[i][j] = nAleatorio;
+                
+            }
+            
+        }
+        
+    }
+
+    //Relleno la matriz con numeros que me va introduciendo el usuario 
+    private static void rellenarConUsuario(int[][] matrizA) {
+        
+        Scanner teclado = new Scanner(System.in);
+        
+        boolean repetir = true;
+        
+        for (int i = 0; i < matrizA.length; i++) {
+            
+            for (int j = 0; j < matrizA[i].length; j++) {
+                
+                do {
+
+                    //Captura la excepcion por si el usuario no mete un numero entero
+                    try {
+                        
+                        System.out.println("Introduce un numero en la posicion: " + i + "| " + j);
+                        
+                        matrizA[i][j] = teclado.nextInt();
+                        
+                        repetir = false;
+                        
+                    } catch (InputMismatchException ime) {
+                        
+                        System.out.println("Por favor introduce un numero entero");
+                        
+                    }
+                    
+                } while (repetir);
+            }
+        }
+        
+    }
+
+    //Metodo para imprimir la matriz creada
+    private static void imprimirMatriz(int[][] matrizA) {
+        
+        for (int i = 0; i < matrizA.length; i++) {
+            
+            for (int j = 0; j < matrizA[i].length; j++) {
+                
+                if (j != matrizA[i].length - 1) {
+                    
+                    System.out.print( matrizA[i][j] + "|");
+                    
+                } else {
+                    
+                    System.out.println( matrizA[i][j] + "|");
+                }
+                
+            }
+            
+        }
+        
+    }
+    
+    // Método para comprobar si es el mayor numero de su fila
+    private static Coordenada mayorFila(int fila, int[][] matrizA) {
+
+        //Nueva cordenada con los valores de fila y columna -1
+        Coordenada coordenada = new Coordenada(-1, -1);
+        
+        int mayorFila = 0;
+        
+        for (int i = 0; i < matrizA.length; i++) {
+            
+            if (matrizA[fila][i] >= mayorFila) {
+
+                //Igualo el valor de mayorFila al numero que supere su valor
+                mayorFila = matrizA[fila][i];
+
+                //Le doy el valor del indice donde he encontrado el mayor
+                //numero a el objeto coordenada
+                coordenada.setFila(fila);
+                coordenada.setColumna(i);
+                
+            }
+            
+        }
+        
+        return coordenada;
+    }
+    
+    
+    // Método para comprobar si es el menor numero de su columna
+    private static boolean esMenor(Coordenada coordenada, int[][] matrizA) {
+        
+        boolean esMenor = true;
+        
+        int aux = matrizA[coordenada.getFila()][coordenada.getColumna()];
+        
+        for (int i = 0; i < matrizA[coordenada.getFila()].length; i++) {
+            
+            if (matrizA[i][coordenada.getColumna()] < aux) {
+                
+                esMenor = false;
+ 
+            }
+        }
+        
+        return esMenor;
+    }
+    
+    //Comprueba si todo coincide y si no devuelve el objeto coordenada con los valores -1 y -1
+    private static Coordenada coordenadaNumero(int[][] matrizA) {
+        
+        Coordenada coordenada = new Coordenada(-1, -1);
+        
+        for (int i = 0; i < matrizA.length; i++) {
+            
+            coordenada = mayorFila(i, matrizA);
+            
+            if (!esMenor(coordenada, matrizA)) {
+                
+                if (i == matrizA.length - 1) {
+                    
+                    coordenada.setFila(-1);
+                    
+                    coordenada.setColumna(-1);
+                    
+                }
+            } 
+            
+        }
+        
+        return coordenada;
     }
     
 }
--- src/main/java/Ej5EDavidMena/EjercicioB.java
+++ src/main/java/Ej5EDavidMena/EjercicioB.java
@@ -4,6 +4,8 @@
  */
 package Ej5EDavidMena;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
 import javax.swing.JOptionPane;
 
 /**
@@ -13,18 +15,117 @@ import javax.swing.JOptionPane;
 public class EjercicioB {
     
     public static void main(String[] args) {
+
+        int tronco = 2; //Le doy un valor al tronco 
+
+        int copaAltitud = copaAltitud(); //Solicito y creo la altura de la copa del arbol
+
+        //Igualo la matriz arbol al metodo para rellenar el arbol
+        String[][] arbol = rellenarCopaArbol(copaAltitud, 2);
+
+        //Imprimo el arbol
+        imprimirMatriz(arbol);
+
+    }
+
+    //Metodo para pedir la altitud de la copa del arbol controlando
+    //que sea mayor que  2 y menor que 10 y que sea un numero entero
+    private static int copaAltitud() {
+
+        Scanner teclado = new Scanner(System.in);
+
+        int copaAltitud = 0;
+
+        do {
+
+            try {
+
+                System.out.println("Introduce la altitud de la copa del arbol");
+                copaAltitud = teclado.nextInt();
+                
+                //Si la altura es superior a 10 o menor a 2 se le notifica al usuario
+                if (copaAltitud < 2 || copaAltitud > 10) {
+
+                    System.out.println("La altura del arbol tiene que estar entre 2 y 10");
+
+                }
+                
+            //Si la altura no es un numero entero se le notifica al usuario
+            } catch (InputMismatchException ime) {
+
+                System.out.println("La altura del arbol tiene que ser un numero entero");
+
+            }
+
+        } while (copaAltitud < 2 || copaAltitud > 10);
+
+        return copaAltitud;
+    }
+
+    private static String[][] rellenarCopaArbol(int copaAltitud, int tronco) {
+
+        //Creo una matriz arbol
         
-        int alturaCopa = 0;
+        //1º Parentesis el numero de filas que sera igual a: 2 de altura base del tronco + la altitud del usuario
         
-        do{
+        //2º Parentesis el ancho es decir las columnas al ancho siempre es la altitud 
+        //que me de el usuario multiplicado por 2 (ya que empieza a contar desde 0 el ancho de la matriz)
+        String[][] arbol = new String[(tronco + copaAltitud)][(copaAltitud * tronco)];
+
+        //Recorro la matriz y relleno la copa con los caracteres " " como espacio y * como relleno de la matriz
+        for (int i = 0; i < arbol.length - tronco; i++) {
+
+            for (int j = 0; j < arbol[i].length; j++) {
+
+                if (j < copaAltitud - i || j > (copaAltitud + i)) {
+
+                    arbol[i][j] = " ";
+
+                } else {
+
+                    arbol[i][j] = "*";
+                }
+
+            }
+        }
+
+        //Relleno la parte del tronco 
         
-            String alturaCopaString = (JOptionPane.showInputDialog("Introduce la altura del arbol(2 - 10)"));
-            alturaCopa = Integer.parseInt(alturaCopaString);
-            
-            int [][] arbolCopa = new int [alturaCopa][3];
-        
-        }while(alturaCopa>10 || alturaCopa<2);
-        
+        //Recorro la matriz y relleno la copa con los caracteres " " como espacio y * como relleno de la matriz
+        for (int i = arbol.length - tronco; i < arbol.length; i++) {
+
+     
+            for (int j = 0; j < arbol[i].length; j++) {
+
+                if (j != copaAltitud) {
+
+                    arbol[i][j] = " ";
+
+                } else {
+
+                    arbol[i][j] = "*";
+
+                }
+
+            }
+        }
+
+        return arbol; //Devuelvo la matriz de arbol ya rellena
+    }
+
+    //Metodo para imprimir la matriz
+    private static void imprimirMatriz(String[][] arbol) {
+
+        for (int i = 0; i < arbol.length; i++) {
+
+            for (int j = 0; j < arbol[i].length; j++) {
+
+                System.out.print(arbol[i][j]);
+
+            }
+
+            System.out.println("");//Mete un salto
+        }
     }
     
 }
